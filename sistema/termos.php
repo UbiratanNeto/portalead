272775086d6bad8478951ee26600c6b86b339468
Ubiratan Neto <ubiratanbsn@gmail.com>
2025-01-12 09:22:26 +0000

    Pagina de termos e condicoes

---
+++ sistema/termos.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Termos e Condições</title>
+</head>
+
+<body>
+    <div class="container"></div>
+</body>
+
+</html>
