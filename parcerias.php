fb3c74f320b3ee7242890123da3477e03d4887c8
ubiratan-neto <ubiratan.neto@drivetel.pt>
2025-11-04 16:30:31 +0000

    Aula 26 - Cards dos planos

---
+++ parcerias.php
@@ -0,0 +1,45 @@
+<?php 
+require_once("cabecalho.php");
+
+?>
+
+
+
+<br>
+<hr>
+
+
+      <section id="about-page-section-3">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-6 col-md-6 col-lg-7 text-align">
+                    <div class="section-heading">
+                        <h2>Parcerias <span>de Cursos</span></h2>
+                        <p class="subheading">Fazemos parcerias com algumas escolas de cursos!</p>
+                    </div>
+                    <p>
+                       Trabalhamos atualmente apenas com contrato de locação de direitos de revenda dos cursos, é um valor fixo mensal com base na quantidade de cursos que desejar trabalhar.
+
+
+                        <br><br> Não trabalhamos com comissões e nem com nenhum outro tipo de parceria, caso tenha interesse <a href="https://drive.google.com/file/d/1FsmzU4qzOzDxlXjLw352ine1GZCaMda6/view?usp=sharing" target="_blank"><span>clique aqui</span></a> para baixar o modelo de contrato no qual trabalhamos.
+                        OBS: O valor mensal varia de acordo com a quantidade de cursos que o contratante deseja trabalhar, atualmente temo um total de <?php echo $cursos ?> cursos. 
+
+
+                    </p>
+                  
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-6 col-lg-5">
+                    <img height="" width="auto" src="img/iphone62.png" class="attachment-full img-responsive" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+
+<br>
+<hr>
+
+
+<?php 
+require_once("rodape.php");
+?>
+
---
+++ planos.php
@@ -0,0 +1,116 @@
+<?php 
+require_once("cabecalho.php");
+
+?>
+
+
+
+<br>
+<hr>
+
+
+      <section id="features-page">
+                <!--pricing-->
+        <div class="subsection4">
+            <div class="container">
+                <div class="col span_12">
+                    <div class="col-sm-12 wpb_column column_container">
+                        <div class="wpb_wrapper">
+                            <div class="section-heading text-center">
+                                <h1>Nossos <span>planos</span></h1>
+                                <p class="subheading">Confira nossos planos de assinaturas.</p>
+                            </div>
+                            <div class="row" style="margin-top:30px;">
+                                <div class="wpb_column vc_column_container col-sm-4 block grow">
+                                    <div class="vc_column-inner">
+                                        <div class="wpb_wrapper">
+                                            <div class="pricing-plan pricing-color-true">
+                                                <div class="pricing-plan-head pricing-plan-1">
+                                                    <h3>Starter Edition</h3>
+                                                    <div class="pricing-price">
+                                                        <span class="pricing-amount">$29.00</span>
+                                                        <span class="pricing-per">per month</span>
+                                                    </div>
+                                                </div>
+                                                <ul>
+                                                    <li><strong>Free</strong> Setup</li>
+                                                    <li><strong>10GB</strong> Storage</li>
+                                                    <li><strong>100GB</strong> Bandwith</li>
+                                                    <li><strong>5</strong> Products</li>
+                                                    <!-- <li><strong>Basic</strong> Stats</li> -->
+                                                </ul>
+                                                <div class="pricing-signup">
+                                                    <a href="#" class="button color-5">Comprar</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="wpb_column vc_column_container col-sm-4 block grow">
+                                    <div class="vc_column-inner">
+                                        <div class="wpb_wrapper">
+                                            <div class="pricing-plan pricing-color-true ">
+                                                <div class="pricing-plan-head pricing-plan-2">
+                                                    <div class="featured-ribbon">Melhor plano</div>
+                                                    <h3>Todos os cursos</h3>
+                                                    <div class="pricing-price">
+                                                        <span class="pricing-amount">R$ 1799,00</span>
+                                                        <span class="pricing-per">Valor total</span>
+                                                    </div>
+                                                </div>
+                                                <ul>
+                                                    <li><strong>Free</strong> Setup</li>
+                                                    <li><strong>100GB</strong> Storage</li>
+                                                    <li><strong>1000GB</strong> Bandwith</li>
+                                                    <li><strong>25</strong> Products</li>
+                                                    <!-- <li><b>Complex</b>&nbsp;Stats</li> -->
+                                                </ul>
+                                                <div class="pricing-signup">
+                                                    <a href="#" class="button color-5">Comprar</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="wpb_column vc_column_container col-sm-4 block grow">
+                                    <div class="vc_column-inner ">
+                                        <div class="wpb_wrapper">
+                                            <div class="pricing-plan pricing-color-true">
+                                                <div class="pricing-plan-head pricing-plan-1">
+                                                    <h3>20 cursos a sua escolha</h3>
+                                                    <div class="pricing-price">
+                                                        <span class="pricing-amount">R$ 399,00</span>
+                                                        <span class="pricing-per">Valor total</span>
+                                                    </div>
+                                                </div>
+                                                <ul>
+                                                    <li><strong>Free</strong> Setup</li>
+                                                    <li><strong>50GB</strong> Storage</li>
+                                                    <li><strong>500GB</strong> Bandwith</li>
+                                                    <li><b>Unlimited</b> Products</li>
+                                                    <!-- <li><b>Complex</b> Stats</li> -->
+                                                </ul>
+                                                <div class="pricing-signup">
+                                                    <a href="#" class="button color-5">Comprar</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    </section>
+
+<br>
+<hr>
+
+
+<?php 
+require_once("rodape.php");
+?>
+
